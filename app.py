5904adcfe38009fec419937076833c7f0af2a9a5
albertgong <84435089+albertgongsun@users.noreply.github.com>
2024-03-27 11:23:54 +0000

    completed lab03

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
+
+from config import config
+
+from forms import loginform
 
 app = Flask(__name__)
+app.config.from_object(config)
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {'username': "Miguel"}
+    user = {"username": "Miguel"}
     posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+        {"author": {"username": "Albert"}, "body": "What is a fucky day!"},
     ]
 
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = loginform()
+    if form.validate_on_submit():
+        flash(
+            "Login requested for user {}, remember_me {}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect("/index")
+    return render_template("login.html", title="Sign In", form=form)
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+
+class config(object):
+
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
